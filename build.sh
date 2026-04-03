11bfc234a74be394c164b9df7799f79537380cfc
fellow99 <fellow99@163.com>
2026-04-03 13:51:01 +0800

    feat: 更新为playwright+sdkman+opencode+openclaw

--- build.sh
+++ build.sh
@@ -1,20 +1,18 @@
-VERSION_PLAYWRIGHT=latest
-VERSION_OPENCODE=1.3.9
-VERSION_OPENCLAW=2026.3.28
-JAVA_MAJOR_VERSION=17
+VERSION_OPENCODE=1.3.13
+VERSION_OPENCLAW=2026.4.2
 
-#docker build -t fellow99/playwright-vnc:$VERSION_PLAYWRIGHT -f Dockerfile-playwright-vnc .
-docker build -t fellow99/playwright-opencode:$VERSION_OPENCODE -f Dockerfile-playwright-opencode --build-arg VERSION_OPENCODE=$VERSION_OPENCODE .
-docker build -t fellow99/playwright-opencode-java$JAVA_MAJOR_VERSION:$VERSION_OPENCODE -f Dockerfile-playwright-opencode-java --build-arg VERSION_OPENCODE=$VERSION_OPENCODE --build-arg JAVA_MAJOR_VERSION=$JAVA_MAJOR_VERSION .
-docker build -t fellow99/playwright-opencode-openclaw:$VERSION_OPENCODE-$VERSION_OPENCLAW -f Dockerfile-playwright-opencode-openclaw --build-arg VERSION_OPENCODE=$VERSION_OPENCODE --build-arg VERSION_OPENCLAW=$VERSION_OPENCLAW .
-docker build -t fellow99/playwright-opencode-openclaw-java$JAVA_MAJOR_VERSION:$VERSION_OPENCODE-$VERSION_OPENCLAW -f Dockerfile-playwright-opencode-openclaw-java --build-arg VERSION_OPENCODE=$VERSION_OPENCODE --build-arg VERSION_OPENCLAW=$VERSION_OPENCLAW --build-arg JAVA_MAJOR_VERSION=$JAVA_MAJOR_VERSION .
+docker builder prune -f
+docker build -t fellow99/playwright-vnc:latest -f Dockerfile-playwright-vnc .
+docker build -t fellow99/playwright-sdkman:latest -f Dockerfile-playwright-sdkman .
+docker build -t fellow99/opencode-aio:$VERSION_OPENCODE -f Dockerfile-opencode-aio --build-arg VERSION_OPENCODE=$VERSION_OPENCODE .
+docker build -t fellow99/opencode-openclaw:$VERSION_OPENCODE-$VERSION_OPENCLAW -f Dockerfile-opencode-openclaw --build-arg VERSION_OPENCODE=$VERSION_OPENCODE --build-arg VERSION_OPENCLAW=$VERSION_OPENCLAW .
 
-#docker save fellow99/playwright-vnc:$VERSION_PLAYWRIGHT > fellow99_playwright-vnc_$VERSION_PLAYWRIGHT.tar
-#docker save fellow99/playwright-opencode:$VERSION_OPENCODE > fellow99_playwright-opencode_$VERSION_OPENCODE.tar
-#docker save fellow99/playwright-opencode-java$JAVA_MAJOR_VERSION:$VERSION_OPENCODE > fellow99_playwright-opencode-java$JAVA_MAJOR_VERSION_$VERSION_OPENCODE.tar
-docker save fellow99/playwright-opencode-openclaw-java$JAVA_MAJOR_VERSION:$VERSION_OPENCODE-$VERSION_OPENCLAW > fellow99_playwright-opencode-openclaw-java${JAVA_MAJOR_VERSION}_$VERSION_OPENCODE-$VERSION_OPENCLAW.tar
+#docker save fellow99/playwright-vnc:latest > fellow99_playwright-vnc_latest.tar
+#docker save fellow99/playwright-sdkman:latest > fellow99_playwright-sdkman_latest.tar
+#docker save fellow99/opencode-aio:$VERSION_OPENCODE > fellow99_opencode-aio_$VERSION_OPENCODE.tar
+#docker save fellow99/opencode-openclaw:$VERSION_OPENCODE-$VERSION_OPENCLAW > fellow99_opencode-openclaw_$VERSION_OPENCODE-$VERSION_OPENCLAW.tar
 
 #gzip fellow99_playwright-vnc_$VERSION.tar
-#gzip fellow99_playwright-opencode_$VERSION_OPENCODE.tar
-#gzip fellow99_playwright-opencode-java$JAVA_MAJOR_VERSION_$VERSION_OPENCODE.tar
-#gzip fellow99_playwright-opencode-openclaw-java${JAVA_MAJOR_VERSION}_$VERSION_OPENCODE-$VERSION_OPENCLAW.tar
+#gzip fellow99_playwright-sdkman_latest.tar
+#gzip fellow99_opencode-aio_$VERSION_OPENCODE.tar
+#gzip fellow99_opencode-openclaw_$VERSION_OPENCODE-$VERSION_OPENCLAW.tar
---
+++ init-sdkman.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk default java 17-open
