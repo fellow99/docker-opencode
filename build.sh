c3349442f87a24aad5519009ae5a89c592aab87f
fellow99 <fellow99@163.com>
2026-07-05 20:21:27 +0800

    feat: 更新到1.17.13

--- build.sh
+++ build.sh
@@ -1,19 +1,22 @@
-VERSION_DEV_IN_ONE=20260510
-VERSION_OPENCODE=1.14.46
+VERSION_DEV_IN_ONE=20260609
+VERSION_OPENCODE=1.17.13
 
 docker builder prune -f
 docker build -t fellow99/dev-in-one:$VERSION_DEV_IN_ONE -f Dockerfile-dev-in-one .
-docker build -t fellow99/dev-playwright:$VERSION_DEV_IN_ONE -f Dockerfile-dev-playwright --build-arg VERSION_DEV_IN_ONE=$VERSION_DEV_IN_ONE .
 docker build -t fellow99/dev-opencode:$VERSION_OPENCODE -f Dockerfile-dev-opencode --build-arg VERSION_DEV_IN_ONE=$VERSION_DEV_IN_ONE --build-arg VERSION_OPENCODE=$VERSION_OPENCODE .
 
 docker push fellow99/dev-in-one:$VERSION_DEV_IN_ONE
-docker push fellow99/dev-playwright:$VERSION_DEV_IN_ONE
 docker push fellow99/dev-opencode:$VERSION_OPENCODE
 
 docker save fellow99/dev-in-one:$VERSION_DEV_IN_ONE > fellow99_dev-in-one_$VERSION_DEV_IN_ONE.tar
-docker save fellow99/dev-playwright:$VERSION_DEV_IN_ONE > fellow99_dev-playwright_$VERSION_DEV_IN_ONE.tar
 docker save fellow99/dev-opencode:$VERSION_OPENCODE > fellow99_dev-opencode_$VERSION_OPENCODE.tar
 
 gzip fellow99_dev-in-one_$VERSION_DEV_IN_ONE.tar
-gzip fellow99_dev-playwright_$VERSION_DEV_IN_ONE.tar
 gzip fellow99_dev-opencode_$VERSION_OPENCODE.tar
+
+
+
+
+docker build -t fellow99/opencode:$VERSION_OPENCODE -f Dockerfile-opencode --build-arg VERSION_OPENCODE=$VERSION_OPENCODE .
+docker build -t fellow99/opencode-design:$VERSION_OPENCODE -f Dockerfile-opencode-design --build-arg VERSION_OPENCODE=$VERSION_OPENCODE .
+docker build -t fellow99/opencode-webapps:$VERSION_OPENCODE -f Dockerfile-opencode-webapps --build-arg VERSION_OPENCODE=$VERSION_OPENCODE .
